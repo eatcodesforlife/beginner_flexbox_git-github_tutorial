aa041a95d81d9beff402a0c1a164bd0971d19061
Cris Acosta <iamcris87@gmail.com>
2018-09-10 19:50:52 +0800

    modified css and script due to responsiveness issue

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 function openSideMenu() {
   document.getElementById('side-menu').style.width='300px';
-  document.getElementById('container-main').style.marginLeft='300px';
+  // document.getElementById('container-main').style.marginLeft='300px';
 }
 
 function closeSideMenu() {
   document.getElementById('side-menu').style.width='0';
-  document.getElementById('container-main').style.marginLeft='0';  
+  // document.getElementById('container-main').style.marginLeft='0';
 }
